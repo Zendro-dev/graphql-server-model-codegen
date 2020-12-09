44379017754d6f582f53d9053f1b932dc73506a0
David Velasco <davvelsan@gmail.com>
2020-12-09 10:18:43 +0100

    chore: remove unused patches and improve readability

--- test/testenv_sync.sh
+++ test/testenv_sync.sh
@@ -12,19 +12,19 @@ printBlockHeader "START" "APPLY CUSTOM PATCHES"
 
 # Apply test-specific patches to the appropriate graphql server instance
 patch -V never \
-  "${GRAPHQL_SERVER_1}/models/generic/aminoacidsequence.js" \
-  "${TEST_DIR}/integration_test_misc/patches/model_aminoacidsequence.patch"
-
-patch -V never \
-  "${GRAPHQL_SERVER_1}/validations/individual.js" \
-  "${TEST_DIR}/integration_test_misc/patches/individual_validate.patch"
-
-patch -V never \
   "${GRAPHQL_SERVER_1}/validations/accession.js" \
   "${TEST_DIR}/integration_test_misc/patches/accession_validate_instance1.patch"
 
 patch -V never \
   "${GRAPHQL_SERVER_1}/models/sql/cat.js" \
   "${TEST_DIR}/integration_test_misc/patches/cat_readAllCursor.patch"
+
+patch -V never \
+  "${GRAPHQL_SERVER_1}/validations/individual.js" \
+  "${TEST_DIR}/integration_test_misc/patches/individual_validate.patch"
+
+patch -V never \
+  "${GRAPHQL_SERVER_1}/models/generic/aminoacidsequence.js" \
+  "${TEST_DIR}/integration_test_misc/patches/model_aminoacidsequence.patch"
 
 printBlockHeader "END" "APPLY CUSTOM PATCHES"
